e19e7c8fe6fdaf74294b1263209a73019272bbfe
neeerajtk <neerajtk560@gmail.com>
2020-09-17 00:06:04 +0530

    basic def of redux

--- r-counter/src/index.js
+++ r-counter/src/index.js
@@ -4,12 +4,17 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
+// STORE - GLOBALIZED STATE 
+
+// ACTION - DESCRIBES WHAT YOU WANT TO DO --> IN THIS APP INCREMENT 
+
+// REDUCER - DESCRIBES HOW THE ACTIONS TRANSFORM THE STATE INTO THE NEXT STATE, THAT IS IT MODIFIES THE STORE DEPENDING ON THE ACTION
+
+// DISPATCH - THIS IS WHERE WE CAN EXECURE THE ACTION, ie,  DISPATCH THE ACTION TO THE REDUCER
+
+
+ReactDOM.render(<App />,document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
