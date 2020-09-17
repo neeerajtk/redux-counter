691b5bd0f3a6306fefc50caf74b346a12f35287b
neeerajtk <neerajtk560@gmail.com>
2020-09-17 11:48:05 +0530

    subscribe issue resolved

--- r-counter/src/index.js
+++ r-counter/src/index.js
@@ -1,18 +1,64 @@
-import React from 'react';
+import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
 
+
+
+// **************************************
 
 // STORE - GLOBALIZED STATE 
 
-// ACTION - DESCRIBES WHAT YOU WANT TO DO --> IN THIS APP INCREMENT 
 
-// REDUCER - DESCRIBES HOW THE ACTIONS TRANSFORM THE STATE INTO THE NEXT STATE, THAT IS IT MODIFIES THE STORE DEPENDING ON THE ACTION
+// **************************************
+// ACTION - DESCRIBES WHAT YOU WANT TO DO --> IN THIS case INCREMENT 
 
+const increment = () => {
+  return {
+    type: 'INCREMENT'
+    //or name: 'INCREMENT', type is the name given    
+  };
+};
+
+const decrement = () => {
+  return {
+    type: 'DECREMENT'
+    //or name: 'DECREMENT', type is the name given    
+  };
+};
+
+// **************************************
+// REDUCER - DESCRIBES HOW THE ACTIONS TRANSFORM THE STATE INTO THE NEXT STATE, 
+// THAT IS IT MODIFIES THE STORE DEPENDING ON THE ACTION
+
+const counter = (state = 0, action) => {
+  // state=0 describes the intial state 
+  switch(action.type){
+    case 'INCREMENT':
+      return state + 1;
+    
+    case 'DECREMENT':
+      return state-1;
+
+    default:
+      return state;
+  }
+}
+
+let store = createStore(counter);
+
+// Displaying it in the console
+
+store.subscribe(() => console.log(store.getState()));
+
+// **************************************
 // DISPATCH - THIS IS WHERE WE CAN EXECURE THE ACTION, ie,  DISPATCH THE ACTION TO THE REDUCER
 
+store.dispatch(increment());
 
 ReactDOM.render(<App />,document.getElementById('root'));
 
