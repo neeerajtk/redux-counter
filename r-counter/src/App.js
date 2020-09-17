d43ac8cd6c8c50cdb77513d2ad68feee27468a97
neeerajtk <neerajtk560@gmail.com>
2020-09-17 18:28:29 +0530

    multiple state printing added

--- r-counter/src/App.js
+++ r-counter/src/App.js
@@ -3,9 +3,12 @@ import {useSelector} from 'react-redux';
 
 function App() {
   const counter = useSelector(state => state.counter);
+  const logged = useSelector(state=>state.isLogged);
   return (
     <div className="App">
       <h1>Counter Value : {counter} </h1>
+
+    {logged ? <h3>You are currently Logged In</h3> : <h3>You are currently Not logged In</h3> }
     </div>
   );
 }
