f3de0405adaba6d49fd12da2f5fef3bdc3021179
neeerajtk <neerajtk560@gmail.com>
2020-09-17 15:05:08 +0530

    redux dev tools added

--- r-counter/src/App.js
+++ r-counter/src/App.js
@@ -4,7 +4,7 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-     
+      <h1>Hello</h1>
     </div>
   );
 }
--- r-counter/src/index.js
+++ r-counter/src/index.js
@@ -6,7 +6,10 @@ import * as serviceWorker from './serviceWorker';
 import {createStore} from 'redux';
 import allReducer from './reducers/index';
 
-const store = createStore(allReducer);
+const store = createStore(allReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() 
+  // above line is copied from redux dev tools github documentation 
+  );
 
 
 ReactDOM.render(<App />,document.getElementById('root'));
